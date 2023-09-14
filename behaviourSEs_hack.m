9ab3924c8b813165eb8cf21b383b96dee395cdc4
dinisov <dgokaydin@gmail.com>
2023-09-14 10:39:03 +1000

    Added hack version

--- behaviourSEs_hack.m
+++ behaviourSEs_hack.m
@@ -13,7 +13,10 @@ files = files(~[files.isdir],:);
 datas = cell(length(files),1);
 
 for i = 1:length(files)
-    datas{i} = table2array(readtable(fullfile('Data',files(i).name)));
+    thisFlyData = table2array(readtable(fullfile('Data',files(i).name)));
+    datas{i} = zeros(size(thisFlyData,1),7);
+    datas{i}(:,1:5) = thisFlyData(:,3:7);
+    datas{i}(:,7) = thisFlyData(:,10);
 end
 
 data = cell2mat(datas);
@@ -21,7 +24,7 @@ data = cell2mat(datas);
 %there are some zero rows for some reason
 data = data(logical(sum(data,2)),:);
 
-data = data(data(:,10) > 0,:);
+data = data(data(:,7) > 0,:);
 
 %% analyse data
 
@@ -32,7 +35,7 @@ auxSeq = [1:16 16:-1:1];
 % get binary value of sequence (up to pattern)
 for trial = 1:size(data,1) 
     % put binary value in 11th column
-    data(trial,11) = auxSeq(bin2dec(num2str(data(trial,3:7) > 0)) + 1);
+    data(trial,6) = auxSeq(bin2dec(num2str(data(trial,1:5) > 0)) + 1);
 end
 
 SEProfile = zeros(16,1);
@@ -40,8 +43,8 @@ SEProfile = zeros(16,1);
 SEMSEs = zeros(16,1);
 
 for s = 1:16
-    SEProfile(s) = mean(data(data(:,11)==s,10));
-    SEMSEs(s) = std(data(data(:,11)==s,10))/sqrt(size(data(data(:,11)==s,10),1));
+    SEProfile(s) = mean(data(data(:,6)==s,7));
+    SEMSEs(s) = std(data(data(:,6)==s,7))/sqrt(size(data(data(:,6)==s,7),1));
 end
 
 create_seq_eff_plot(SEProfile(seq_eff_order(5)),[],'errors',SEMSEs);
